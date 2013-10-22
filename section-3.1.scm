b755fbc46fd5ca4ca3ffd6eefbe76b1adb07352e
lmc_chun@msn.com <lmc_chun@msn.com>
2013-10-23 01:34:46 +0800

    update

--- section-3.1.scm
+++ section-3.1.scm
@@ -112,3 +112,94 @@
        (lambda (num)
 	 (set! init (* init num))
 	 init)))))
+
+; ex 3.12
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+; ex 3.13
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+; ex 3.14
+(define (mystery x)
+  (letrec ((loop (lambda (x y)
+		   (if (null? x)
+		       y
+		       (let ((temp (cdr x)))
+			 (set-cdr! x y)
+			 (loop temp x))))))
+    (loop x '())))
+
+; ex 3.17
+(define (count-pairs x)
+  (letrec ((inner (lambda (x memo-list)
+		    (if (and (pair? x)
+			     (not (memq x memo-list)))
+			(inner (car x)
+			       (inner (cdr x)
+				      (cons x memo-list)))
+			memo-list))))
+    (length (inner x '()))))
+
+; ex 3.18
+(define (has-loop? x)
+  (letrec ((traverse-list (lambda (x memo-list)
+			    (if (pair? x)
+				(if (memq x memo-list)
+				    #t
+				    (let ((new-memo-list (cons x memo-list)))
+				      (or (traverse-list (car x) new-memo-list) ; XXX
+					  (traverse-list (cdr x) new-memo-list))))
+				#f))))
+    (traverse-list x '())))
+
+; ex 3.19
+; ???
+(define (o1-space-has-loop? x)
+  (letrec ((traverse-sub-list (lambda (start cur step)
+				(if (eq? start cur)
+				    #t
+				    (if (or (not (pair? start)) (= step 1))
+					#f
+					(traverse-sub-list (cdr start) cur (- step 1)))))))
+    (letrec ((traverse-list (lambda (cur count)
+			      (if (traverse-sub-list x cur count)
+				  #t
+				  (if (pair? cur)
+				      (traverse-list (cdr cur) (+ count 1))
+				      #f)))))
+      (if (pair? x)
+	  (traverse-list (cdr x) 1)
+	  #f))))
+
+; 使用两个变量, 一个变量以步长为 1 遍历列表, 另一个变量以步长为 2 遍历列表,
+; 每次在两个变量移动之后对比它们, 如果两个变量相遇，那么列表有环;
+; 如果能走完整个列表(遇到 '() ), 那么列表没有环.
+(define (another-o1-space-has-loop? lst)
+  (define (iter x y)
+    (let ((x-walk (list-walk 1 x))
+	  (y-walk (list-walk 2 y)))
+      (cond ((or (null? x-walk) (null? y-walk))
+	     #f)
+	    ((eq? x-walk y-walk)
+	     #t)
+	    (else
+	     (iter x-walk y-walk)))))
+  (iter lst lst))
+
+(define (list-walk step lst)
+  (cond ((null? lst)
+	 '())
+	((= step 0)
+	 lst)
+	(else
+	 (list-walk (- step 1)
+		    (cdr lst)))))
