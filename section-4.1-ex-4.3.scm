cb60dd7a0358a84dd9d577fa10d1eabc7e5ca852
lmc_chun@msn.com <lmc_chun@msn.com>
2014-04-01 01:50:32 +0800

    update

--- section-4.1-ex-4.3.scm
+++ section-4.1-ex-4.3.scm
@@ -27,3 +27,125 @@
 (define get (operation-table 'lookup-proc))
 
 (define put (operation-table 'insert-proc!))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
+
+(define (text-of-quotation exp)
+  (cadr exp))
+
+(define (eval-quoted exp)
+  (text-of-quotation exp))
+
+(define (assignment-variable exp)
+  (cadr exp))
+
+(define (assignment-value exp)
+  (caddr exp))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+		       (eval (assignment-value exp) env)
+		       env))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cadar exp) ; formal parameters
+		   (cddr exp)))) ; body
+
+(define (eval-definition exp env)
+  (define-variable!
+    (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
+
+(define (if-predicate exp)
+  (cadr exp))
+
+(define (if-consequent exp)
+  (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false)) ; 'false or #f
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define (lambda-parameters exp)
+  (cadr exp))
+
+(define (lambda-body exp)
+  (cddr exp))
+
+(define (eval-lambda exp env)
+  (make-procedure
+   (lambda-parameters exp)
+   (lambda-body exp)
+   env))
+
+(define (last-exp? seq)
+  (null? (cdr seq)))
+
+(define (first-exp seq)
+  (car seq))
+
+(define (rest-exps seq)
+  (cdr seq))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+	(else (eval (first-exp exps) env)
+	      (eval-sequence (rest-exps exps) env))))
+
+(define (eval-begin exp env)
+  (eval-sequence (begin-actions exp) env))
+
+(put 'quote eval-quoted)
+(put 'set! eval-assignment)
+(put 'define eval-definition)
+(put 'if eval-if)
+(put 'lambda eval-lambda)
+(put 'begin eval-begin)
+(put 'cond eval-cond)
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) #t)
+	((string? exp) #t)
+	(else #f)))
+
+(define (variable? exp)
+  (symbol? exp))
+
+(define (application? exp)
+  (tagged-list? exp 'call))
+
+(define (operator exp)
+  (cadr exp))
+
+(define (operands exp)
+  (cddr exp))
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp)
+	 (lookup-variable-value exp env))
+	((get (car exp))
+	 ((get (car exp)) exp env))
+	((application? exp)
+	 (apply (eval (operator exp) env)
+		(list-of-values (operands exp) env)))
+	(else
+	 (error "Unknown expression in EVAL: " exp))))
