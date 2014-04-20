758bcdc34072bf2731229e0496e3c07f4a7426a9
lmc_chun@msn.com <lmc_chun@msn.com>
2014-04-20 22:36:46 +0800

    update

--- section-4.1.scm
+++ section-4.1.scm
@@ -1,3 +1,5 @@
+(define apply-in-underlying-scheme apply) ; 保存基础 apply 的一个引用
+
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
 	((variable? exp) (lookup-variable-value exp env))
@@ -448,7 +450,7 @@
 
 (define the-empty-environment '())
 
-(define (make-framme variables values)
+(define (make-frame variables values)
   (cons variables values))
 
 (define (frame-variables frame)
@@ -535,3 +537,70 @@
 
 (define (unbind-variable! exp env)
   (unbind-var-in-frame! (first-frame env) (cadr exp)))
+
+;
+(define (setup-environment)
+  (let ((initial-env
+	 (extend-environment (primitive-procedure-names)
+			     (primitive-procedure-objects)
+			     the-empty-environment)))
+    (define-variable! 'true #t initial-env)
+    (define-variable! 'false #f initial-env)
+    initial-env))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc)
+  (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car car)
+	(list 'cdr cdr)
+	(list 'cons cons)
+	(list 'null? null?)
+	(list '+ +)
+	(list '- -)
+	(list '* *)
+	(list '/ /)
+	; 其他基本过程
+	))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlying-scheme
+   (primitive-implementation proc) args))
+
+(define input-prompt ";;; M-Eval input: ")
+
+(define output-prompt ";;; M-Eval value: ")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+		     (procedure-parameters object)
+		     (procedure-body object)
+		     '<procedure-env>))
+      (display object)))
+
+(define the-global-environment (setup-environment))
