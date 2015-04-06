dee784a031701a6daf91d09d3869ddef89f93319
lmc <lmc@linux.site>
2015-04-06 17:17:25 +0800

    update

---
+++ section-4.1.rkt
@@ -0,0 +1,103 @@
+#lang racket
+
+(define (eval* exp env)
+  (cond
+    [(self-evaluation? exp) exp]
+    [(variable? exp) (lookup-variable-value exp env)]
+    [(euoted? exp) (text-of-quotation exp)]
+    [(assignment? exp) (eval-assignment exp env)]
+    [(definition? exp) (eval-definition exp env)]
+    [(if? exp) (eval-if exp env)]
+    [(lambda? exp)
+     (make-procedure (lambda-parameters exp)
+                     (lambda-body exp)
+                     env)]
+    [(begin? exp)
+     (eval-sequence (begin-actions exp) env)]
+    [(cond? exp) (eval* (cond->if exp) env)]
+    [(application? exp)
+     (apply* (eval* (operatorexp) env)
+             (list-of-values (operands exp) env))]
+    [else
+     (error "Unknown expression type -- EVAL" exp)]))
+
+(define (apply* procedure arguments)
+  (cond
+    [(primitive-procedure? procedure)
+     (apply-primitive-procedure procedure arguments)]
+    [(compound-procedure? procedure)
+     (eval-sequence
+      (procedure-body procedure)
+      (extend-environment
+       (procedure-parameters procedure)
+       arguments
+       (procedure-environment procedure)))]
+    [else
+     (error "Unknown procedure type -- APPLY" procedure)]))
+
+(define (list-of-values exps env)
+  (if (no-operands? exp)
+      '()
+      (cons (eval* (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval* (if-predicate exp) env))
+      (eval* (if-consequent exp) env)
+      (eval* (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond
+    [(last-exp? exps) (eval* (first-exp exps) env)]
+    [else (eval* (first-exp exps) env)
+          (eval-sequence (rest-exps exps) env)]))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval* (assignment-value exp) env)
+                       env))
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (eval* (definition-value exp) env)
+    env)
+  'ok)
+
+(define (self-evaluating? exp)
+  (cond [(number? exp) #t]
+        [(string? exp) #t]
+        [else #f]))
+
+(define (variable? exp)
+  (symbol? exp))
+
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp)
+  (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
+
+(define (assignment-variable exp)
+  (cadr exp))
+
+(define (assignment-value exp)
+  (caddr exp))
+
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp) ; formal parameters
+                   (cddr exp)))) ; body
