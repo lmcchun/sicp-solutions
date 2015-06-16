68fd3e35b25a242a5d70d522693f08d52b34f8e1
lmc_chun@msn.com <lmc_chun@msn.com>
2015-06-16 21:59:08 +0800

    update

--- section-4.1.7.scm
+++ section-4.1.7.scm
@@ -4,10 +4,35 @@
   (display value)
   (display #\newline))
 
+(define (eval* exp env)
+  ((analyze exp) env))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
 (define (self-evaluating? exp)
   (cond ((number? exp) #t)
-       ((string? exp) #t)
-       (else #f)))
+        ((string? exp) #t)
+        (else #f)))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+(define (analyze-variable exp)
+  (lambda (env)
+    (lookup-variable-value exp env)))
 
 (define (variable? exp)
   (symbol? exp))
@@ -15,22 +40,58 @@
 (define (quoted? exp)
   (tagged-list? exp 'quote))
 
+(define (text-of-quotation exp)
+  (cadr exp))
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation txp)))
+    (lambda (env) qval)))
+
 (define (tagged-list? exp tag)
   (if (pair? exp)
-     (eq? (car exp) tag)
-     #f))
+      (eq? (car exp) tag)
+      #f))
 
 (define (assignment? exp)
   (tagged-list? exp 'set!))
 
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env)
+      (set-variable-value! var (vproc env) env)
+      'ok)))
+
 (define (definition? exp)
   (tagged-list? exp 'define))
+
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env)
+      (define-variable! var (vproc env) env)
+      'ok)))
 
 (define (lambda? exp)
   (tagged-list? exp 'lambda))
 
+(define (analyze-lambda exp)
+  (let ((vars (lambda-parameters exp))
+        (bproc (analyze-sequence (lambda-body exp))))
+    (lambda (env)
+      (make-procedure vars bproc env))))
+
 (define (if? exp)
   (tagged-list? exp 'if))
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env)
+      (if (true? (pproc env))
+          (cproc env)
+          (aproc env)))))
 
 (define (begin? exp)
   (tagged-list? exp 'begin))
@@ -41,23 +102,59 @@
 (define (application? exp)
   (pair? exp))
 
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (execute-application (fproc env)
+                           (map (lambda (aproc) (aproc env))
+                                aprocs)))))
+
+(define (execute-application proc args)
+  (cond ((primitive-procedure? proc)
+         (apply-primitive-procedure proc args))
+        ((compound-procedure? proc)
+         (extend-environment (procedure-parameters proc)
+                             args
+                             (procedure-environment proc)))
+        (else
+         (error
+          "Unknown procedure type -- EXECUTE-APPLICATION"
+          proc))))
+
 (define (cond? exp)
   (tagged-list? exp 'cond))
 
-(define (eval* exp env)
-  ((analyze exp) env))
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env)
+      (proc1 env)
+      (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE")
+        (loop (car procs) (cdr procs)))))
 
-(define (analyze exp)
-  (cond ((self-evaluating? exp)
-         (analyze-self-evaluating exp))
-       ((quoted? exp) (analyze-quoted exp))
-       ((variable? exp) (analyze-variable exp))
-       ((assignment? exp) (analyze-assignment exp))
-       ((definition? exp) (analyze-definition exp))
-       ((if? exp) (analyze-if exp))
-       ((lambda? exp) (analyze-lambda exp))
-       ((begin? exp) (analyze-sequence (begin-actions exp)))
-       ((cond? exp) (analyze (cond->if exp)))
-       ((application? exp) (analyze-application exp))
-       (else
-        (error "Unknown expression type -- ANALYZE" exp))))
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (let ((val
+                 (scan (frame-variables frame)
+                       (frame-values frame))))
+            (if (eq? val '*unassigned*) ;; ex 4.17
+                (error "Using an unassigned variable" var)
+                val)))))
+  (env-loop env))
